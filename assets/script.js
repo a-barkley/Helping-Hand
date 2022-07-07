1fd26fea9d1a78e21458c8f7e2d441e717470110
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-07-07 09:34:06 -0700

    Added Google Geo key

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 var apiKey = "fbd3cad63742864f43fb09168db55be3"
 
+var geoKey = "AIzaSyCwQ3_jDoe6wPIx_Ta8yKhGFEcjmfmXtjw"
+
 function testAPI() {
     fetch("https://api.allorigins.win/raw?url=http://data.orghunter.com/v1/charitysearch?user_key=" + apiKey + "&zipCode=60626")
     .then(function(response) {
