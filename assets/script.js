4b6410bfc99ee910b2c171eae95c565322096654
pbarkley_desk <rpbarkley99@gmail.com>
2022-07-07 18:48:26 -0500

    finish header in tailwind

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,20 @@
-var apiKey = "fbd3cad63742864f43fb09168db55be3"
+var charityKey = "fbd3cad63742864f43fb09168db55be3"
 
 var geoKey = "AIzaSyCwQ3_jDoe6wPIx_Ta8yKhGFEcjmfmXtjw"
 
-function testAPI() {
-    fetch("https://api.allorigins.win/raw?url=http://data.orghunter.com/v1/charitysearch?user_key=" + apiKey + "&zipCode=60626")
+// function testAPI() {
+//     fetch("https://api.allorigins.win/raw?url=http://data.orghunter.com/v1/charitysearch?user_key=" + charityKey + "&zipCode=60626")
+//     .then(function(response) {
+//         console.log(response)
+//         response.json().then(function (data) {
+//             console.log(data)
+//         })
+//     })
+// }
+
+
+function testGeoAPI() {
+    fetch("https://cors-anywhere.herokuapp.com/corsdemo=https://www.googleapis.com/geolocation/v1/geolocate?key=" + geoKey)
     .then(function(response) {
         console.log(response)
         response.json().then(function (data) {
@@ -11,5 +22,5 @@ function testAPI() {
         })
     })
 }
-
-testAPI()
+// testAPI()
+testGeoAPI()
