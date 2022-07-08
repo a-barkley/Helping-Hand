04347e3a05ab53bd1dc22c8f7d5bf087ce6d1000
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-07-07 17:04:42 -0700

    Linked Zipcode Button

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,17 @@ var charityKey = "fbd3cad63742864f43fb09168db55be3"
 
 var geoKey = "AIzaSyCwQ3_jDoe6wPIx_Ta8yKhGFEcjmfmXtjw"
 
-// function testAPI() {
-//     fetch("https://api.allorigins.win/raw?url=http://data.orghunter.com/v1/charitysearch?user_key=" + charityKey + "&zipCode=60626")
-//     .then(function(response) {
-//         console.log(response)
-//         response.json().then(function (data) {
-//             console.log(data)
-//         })
-//     })
-// }
+var userInput = $("#search")
+
+function testAPI() {
+    fetch("https://api.allorigins.win/raw?url=http://data.orghunter.com/v1/charitysearch?user_key=" + charityKey + "&zipCode=" + userInput)
+    .then(function(response) {
+        console.log(response)
+        response.json().then(function (data) {
+            console.log(data)
+        })
+    })
+}
 
 
 function testGeoAPI() {
@@ -22,5 +24,8 @@ function testGeoAPI() {
         })
     })
 }
-// testAPI()
+
+$("#searchBtn").click(testAPI())
+
 testGeoAPI()
+
