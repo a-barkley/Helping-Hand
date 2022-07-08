5fcbcd70d81f4eea3f7984478a04311939099b12
Sean Oh <seanoh@ucsb.edu>
2022-07-08 11:37:55 -0700

    grab data from charities

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,7 @@ function charityAPI(e) {
         })
     .then(function(data){
         console.log(JSON.parse(data.contents))
+        displayCards(JSON.parse(data.contents).data);
     });
 }
 
@@ -28,19 +29,34 @@ function currentLocation(e){
     e.preventDefault();
     navigator.geolocation.getCurrentPosition(function(position) {
         console.log("position", position.coords.latitude, position.coords.longitude);
-    var userLatitude = position.coords.latitude;
-    var userLongitude = position.coords.longitude;
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://data.orghunter.com/v1/charitysearch?user_key=fbd3cad63742864f43fb09168db55be3&latitude=' + userLatitude + '&longitude=' + userLongitude)}`)
-    .then(response => {
-	    if (response.ok) return response.json()
-	        throw new Error('Network response was not ok.')
-        })
-    .then(function(data){
-        console.log(JSON.parse(data.contents))
-    });
+        var userLatitude = position.coords.latitude;
+        var userLongitude = position.coords.longitude;
+        fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://data.orghunter.com/v1/charitysearch?user_key=fbd3cad63742864f43fb09168db55be3&latitude=' + userLatitude + '&longitude=' + userLongitude)}`)
+        .then(response => {
+	        if (response.ok) return response.json()
+	            throw new Error('Network response was not ok.')
+            })
+        .then(function(data){
+            console.log(JSON.parse(data.contents))
+            displayCards(JSON.parse(data.contents).data);
+        });
     })
     
 
+}
+
+function displayCards(data) {
+    console.log(data)
+    for(var i = 0; i < data.length; i++){
+        // get charity data
+        var name = data[i].charityName;
+        var url = data[i].url;
+        var category = data[i].category;
+        var missionStatement = data[i].missionStatement;
+
+        
+
+    }
 }
 
 $("#locationBtn").on("click",currentLocation)
